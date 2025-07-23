c14baa0b498de062f1ba0c503add89d0b1d74388
timishot <timidotcom58@gmail.com>
2025-07-24 00:56:29 +0100

    fix push

--- event_backend/event_backend/asgi.py
+++ event_backend/event_backend/asgi.py
@@ -1,38 +1,38 @@
-#
-# import os
-# import django
-# from django.core.asgi import get_asgi_application
-# from channels.routing import ProtocolTypeRouter, URLRouter
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_backend.settings')
-# django.setup()
-#
-# from core.token_auth import TokenAuthMiddleware
-# import event.routing
-#
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": TokenAuthMiddleware(
-#         URLRouter(
-#             event.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
 
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_backend.settings')
+django.setup()
+
 from core.token_auth import TokenAuthMiddleware
 import event.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_backend.settings')
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        TokenAuthMiddleware(
-            URLRouter(event.routing.websocket_urlpatterns)
+    "websocket": TokenAuthMiddleware(
+        URLRouter(
+            event.routing.websocket_urlpatterns
         )
     ),
 })
+
+# import os
+# from django.core.asgi import get_asgi_application
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
+# from core.token_auth import TokenAuthMiddleware
+# import event.routing
+#
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'event_backend.settings')
+#
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AllowedHostsOriginValidator(
+#         TokenAuthMiddleware(
+#             URLRouter(event.routing.websocket_urlpatterns)
+#         )
+#     ),
+# })
