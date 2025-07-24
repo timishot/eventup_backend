468de3d1fac3abf5f037a7eb5e6d52b5e3b0e1e0
timishot <timidotcom58@gmail.com>
2025-07-24 02:11:25 +0100

    fix push

--- event_backend/event_backend/settings.py
+++ event_backend/event_backend/settings.py
@@ -202,7 +202,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(os.environ.get("REDIS_HOST", "redis"), 6379)],
+            "hosts": [os.environ.get("REDIS_HOST", "redis")],
         },
     },
 }
