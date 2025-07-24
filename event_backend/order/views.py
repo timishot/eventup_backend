b489063353c24b98a43f8ed66068fdb4a81daac3
timishot <timidotcom58@gmail.com>
2025-07-24 15:02:38 +0100

    fix

--- event_backend/order/views.py
+++ event_backend/order/views.py
@@ -16,7 +16,7 @@ def create_order(request):
 
         # Ensure required fields exist
         stripe_id = data.get('stripeId')
-        total_amount = data.get('total_amount')
+        total_amount = data.get('totalAmount')
         event_id = data.get('event_id')
         buyer_id = data.get('buyer_id')
 
