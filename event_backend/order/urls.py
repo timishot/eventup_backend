6aa5041296199d9d95fb4195c4cf695fa6b0417b
timishot <timidotcom58@gmail.com>
2025-07-25 19:47:54 +0100

    fix push

--- event_backend/order/urls.py
+++ event_backend/order/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import create_order, get_user_orders
+from .views import create_order, get_user_orders, get_orders_by_event
 
 urlpatterns = [
     path('create/', create_order, name='create_order'),
     path('orderlist/', get_user_orders, name='get_user_orders'),
+    path('event-orders/', get_orders_by_event, name='get_orders_by_event'),
 ]
--- event_backend/order/views.py
+++ event_backend/order/views.py
@@ -1,3 +1,4 @@
+from django.db.models.functions import Concat
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
@@ -7,6 +8,7 @@ from .models import Order
 from .serializers import OrderSerializer
 from event.models import Event
 from useraccount.models import User  # or get_user_model() if using custom user
+from django.db.models import F, Value
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
@@ -59,3 +61,25 @@ def get_user_orders(request):
         'data': serializer.data,
         'totalPages': (total_count + limit - 1) // limit
     })
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_orders_by_event(request):
+    event_id = request.query_params.get('eventId')
+    search_string = request.query_params.get('searchString', '')
+    try:
+        if not event_id:
+            return Response({"detail": "Event ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        orders = Order.objects.filter(event_id=event_id).select_related('event', 'buyer').order_by('-created_at')
+
+        if search_string:
+            orders = orders.filter(
+                buyer__name__icontains=search_string
+            )
+
+        serializer = OrderSerializer(orders, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        return Response({"detail": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
